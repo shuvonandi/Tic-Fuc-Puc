8bbef71c4ed85c591c11ddbe850bd899e05f3316
shuvonandi <90299916+shuvonandi@users.noreply.github.com>
2024-02-20 15:36:31 +0600

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import Ticfucpus from './Components/TicFukPus/Ticfucpus';
+
+function App() {
+  return (
+    <div>
+      <Ticfucpus/>
+    </div>
+  );
+}
+
+export default App;
